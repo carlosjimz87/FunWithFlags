eccf386eb1ecaa1d59913d685af3df0d23eb25ee
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-23 15:57:22 +0200

    Add livedata,viewmodel, fonts and resources

---
+++ app/src/main/java/com/carlosjimz87/funwithflags/fragments/list/ListFragment.kt
@@ -0,0 +1,25 @@
+package com.carlosjimz87.funwithflags.fragments.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.carlosjimz87.funwithflags.R
+
+class ListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+    }
+}
