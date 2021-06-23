13a465d8b2cb6a102722f563f77f2545badfc6ac
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-23 16:04:38 +0200

    Add NavController

--- app/src/main/java/com/carlosjimz87/funwithflags/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.carlosjimz87.funwithflags
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.tag("FUN_WITH_FLAGS").i("Activity Created!");
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 }
