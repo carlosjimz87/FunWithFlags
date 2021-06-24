a765096e181d47bce92f2c045a172769d45272c6
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-24 16:46:31 +0200

    remove unused file

--- app/src/main/java/com/carlosjimz87/funwithflags/utils/Resources.kt
+++
@@ -1,6 +0,0 @@
-package com.carlosjimz87.funwithflags.utils
-
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
-import androidx.core.content.res.ResourcesCompat
