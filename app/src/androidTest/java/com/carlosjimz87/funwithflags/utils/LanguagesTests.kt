3240ceb813f7553cfe35c51384f6b6a91b65ac94
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-25 08:33:19 +0200

    Adding Languages detection

---
+++ app/src/androidTest/java/com/carlosjimz87/funwithflags/utils/LanguagesTests.kt
@@ -0,0 +1,25 @@
+package com.carlosjimz87.funwithflags.utils
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class LanguagesTests {
+    private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Test
+    fun testModifyLocale() {
+        val locales = listOf("en", "es", "de", "it", "fr", "pt", "fa", "ja")
+        for (newLocale in locales) {
+            Languages.setLocale(appContext, newLocale)
+            val currentLocale = Languages.getLocale(appContext)
+            Assert.assertEquals(newLocale, currentLocale)
+        }
+    }
+}
---
+++ app/src/main/java/com/carlosjimz87/funwithflags/utils/Languages.kt
@@ -0,0 +1,13 @@
+package com.carlosjimz87.funwithflags.utils
+
+import android.content.Context
+import java.util.*
+
+class Languages {
+    companion object {
+        fun getLocale(context: Context): String = context.resources.configuration.locale.language
+
+        fun setLocale(context: Context, lang: String) =
+            context.resources.configuration.setLocale(Locale(lang))
+    }
+}
