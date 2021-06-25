0c85022c0370bcd9f6a778aada8f21abde3c8cb8
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-25 11:39:56 +0200

    Add GoogleMap

--- app/src/main/java/com/carlosjimz87/funwithflags/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/fragments/details/DetailsFragment.kt
@@ -7,7 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.carlosjimz87.funwithflags.R
 import com.carlosjimz87.funwithflags.databinding.DetailsFragmentBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import timber.log.Timber
 
 class DetailsFragment : Fragment() {
@@ -34,6 +40,20 @@ class DetailsFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             viewModel = detailsViewModel
         }
+        setupGoogleMap()
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setupGoogleMap() {
+//        val mapFragment = binding?.map as SupportMapFragment?
+        val mapFragment = fragmentManager?.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     override fun onDestroyView() {
