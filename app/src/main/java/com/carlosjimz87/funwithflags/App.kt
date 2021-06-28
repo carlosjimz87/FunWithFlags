b2f9a4065e1e7788f7396d32431b3f46f3195049
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-28 16:48:44 +0200

    Prepare dependencies and network state access

--- app/src/main/java/com/carlosjimz87/funwithflags/App.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/App.kt
@@ -1,16 +1,35 @@
 package com.carlosjimz87.funwithflags
 
 import android.app.Application
-import com.carlosjimz87.funwithflags.db.CountriesDatabase
+import android.net.ConnectivityManager
 import timber.log.Timber
 
-class App : Application() {
-    val database: CountriesDatabase by lazy { CountriesDatabase.getDatabase(this) }
 
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+        if (instance == null) {
+            instance = this
+        }
+    }
+
+    private val isNetworkConnected: Boolean
+        get() {
+            val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = cm.activeNetworkInfo
+            return activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting
+        }
+
+    companion object {
+        var instance: App? = null
+            private set
+
+        fun hasNetwork(): Boolean {
+            return instance!!.isNetworkConnected
         }
     }
 }
