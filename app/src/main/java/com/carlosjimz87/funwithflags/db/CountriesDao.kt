d0e8217a428d4f102278dd63dade7255339824fd
Carlos Jiménez <carlosjimz87@gmail.com>
2021-06-28 16:30:52 +0200

    add insert and delete interfaces

--- app/src/main/java/com/carlosjimz87/funwithflags/db/CountriesDao.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/db/CountriesDao.kt
@@ -1,6 +1,8 @@
 package com.carlosjimz87.funwithflags.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.carlosjimz87.funwithflags.db.models.CountryEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,6 +12,12 @@ interface CountriesDao {
 
     @Query("SELECT * FROM countries ORDER BY alpha3Code")
     fun getAll(): Flow<List<CountryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(countries: List<CountryEntity>)
+
+    @Query("DELETE FROM countries")
+    fun deleteAll()
 
     @Query("SELECT * FROM countries WHERE alpha3Code = :code ORDER BY alpha3Code")
     fun getByCode(code: String): Flow<CountryEntity>
