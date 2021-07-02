4873f682cf92acf73f6cba6360fd520c4c49dd4f
Carlos Jiménez <carlosjimz87@gmail.com>
2021-07-02 11:45:14 +0200

    replacing UI states with viewbinding operations

--- app/src/main/java/com/carlosjimz87/funwithflags/adapters/BindingAdapters.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/adapters/BindingAdapters.kt
@@ -1,13 +1,9 @@
 package com.carlosjimz87.funwithflags.adapters
 
 import android.graphics.drawable.Drawable
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.carlosjimz87.funwithflags.fragments.CountriesApiStatus
 import com.carlosjimz87.funwithflags.network.models.Country
 import com.carlosjimz87.funwithflags.utils.loadImage
 import timber.log.Timber
@@ -31,45 +27,5 @@ fun bindRecyclerView(
         Timber.i("Submitting new list (${data.size} countries)")
         val adapter = recyclerView.adapter as CountryListAdapter
         adapter.submitAllLists(data.toMutableList(), data.toMutableList())
-    }
-}
-
-@BindingAdapter("activeViewState")
-fun activeViewState(
-    view: View,
-    status: CountriesApiStatus?,
-) {
-    showLoadingState(activeView = view, status = status)
-}
-
-
-@BindingAdapter("inactiveViewState")
-fun inactiveViewState(
-    view: View,
-    status: CountriesApiStatus?,
-) {
-    showLoadingState(inactiveView = view, status = status)
-}
-
-fun showLoadingState(
-    status: CountriesApiStatus?,
-    activeView: View? = null,
-    inactiveView: View? = null,
-    activeLayout: ViewGroup? = null,
-    inactiveLayout: ViewGroup? = null,
-) {
-    when (status) {
-        CountriesApiStatus.SUCCESS -> {
-            activeView?.visibility = View.VISIBLE
-            activeLayout?.visibility = View.VISIBLE
-            inactiveView?.visibility = View.GONE
-            inactiveLayout?.visibility = View.GONE
-        }
-        else -> {
-            activeLayout?.visibility = View.GONE
-            activeView?.visibility = View.GONE
-            inactiveLayout?.visibility = View.VISIBLE
-            inactiveView?.visibility = View.VISIBLE
-        }
     }
 }
--- app/src/main/java/com/carlosjimz87/funwithflags/fragments/list/ListViewModel.kt
+++ app/src/main/java/com/carlosjimz87/funwithflags/fragments/list/ListViewModel.kt
@@ -1,11 +1,9 @@
 package com.carlosjimz87.funwithflags.fragments.list
 
-import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.recyclerview.widget.RecyclerView
 import com.carlosjimz87.funwithflags.fragments.CountriesApiStatus
 import com.carlosjimz87.funwithflags.network.models.Country
 import com.carlosjimz87.funwithflags.repositories.CountriesRepository
